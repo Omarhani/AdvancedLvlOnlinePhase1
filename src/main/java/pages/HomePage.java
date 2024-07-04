0ca2c0769b3718a8408646e7c5b8d02a5d1af5cf
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2024-07-05 02:32:08 +0300

    first push

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,6 +1,5 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.MethodHandles;
 
@@ -10,26 +9,5 @@ public class HomePage extends MethodHandles {
         super(driver);
     }
 
-    private final By loginLink = By.cssSelector(".ico-login");
-    private final By computersMenu = By.cssSelector(".top-menu.notmobile >li > a");
 
-    private final By noteBookLink = By.xpath("(//a[contains(text(),'Notebooks ')])[1]");
-
-    public LoginPage clickOnLoginLink(){
-        click(loginLink,5);
-        return new LoginPage(driver);
-    }
-
-    private void hoverOverComputers(){
-        hoverOverElement(computersMenu,4);
-    }
-    private void clickOnNoteBookLink(){
-        click(noteBookLink,6);
-    }
-
-    public NoteBookPage moveToNoteBookPage(){
-        hoverOverComputers();
-        clickOnNoteBookLink();
-        return new NoteBookPage(driver);
-    }
 }
