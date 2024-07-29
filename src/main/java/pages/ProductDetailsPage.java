ff3cd186681edc42bb91fc93e23042fc9a3a30ab
EmanMonem <emanmonem1014@gmail.com>
2024-07-29 17:12:51 +0300

    Test Case 13: Verify Product quantity in Cart

--- src/main/java/pages/ProductDetailsPage.java
+++ src/main/java/pages/ProductDetailsPage.java
@@ -12,6 +12,10 @@ public class ProductDetailsPage extends MethodHandles {
     private final By FirstProductsLink = By.xpath("//a[@href='/product_details/1']");
     private final By VerifyProductInformation = By.xpath("//div[@class='product-information']/p[contains(text(),'Category')]");
 
+    private final By quantityField = By.id("quantity");
+    private final By addToCartButton = By.cssSelector(".btn.btn-default.cart");
+    private final By viewCartLink = By.xpath("//u[contains(text(),'View Cart')]");
+
     public void clickFirstProductsLink() {
         scrollIntoElement(driver, FirstProductsLink);
         click(FirstProductsLink, 5);
@@ -23,6 +27,19 @@ public class ProductDetailsPage extends MethodHandles {
 
     public String verifyProductInformationDisplay() {
         return getText(VerifyProductInformation, 5);
+    }
+
+    public void insertQuantityField(String quantity){
+        clear(quantityField,5);
+        sendKeys(quantityField,5,quantity);
+    }
+
+    public void clickOnAddToCartButton(){
+        click(addToCartButton , 5);
+    }
+    public ViewCartPage clickOnViewCartLink(){
+        click(viewCartLink,5);
+        return new ViewCartPage(driver);
     }
 
 }
